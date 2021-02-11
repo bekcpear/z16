e9b1688dfe3377963e04f270893f31a51d9e09be
bekcpear <i@bitbili.net>
2021-02-12 01:42:29 +0800

    update readme

--- .tool/convert_readme.sh
+++ .tool/convert_readme.sh
@@ -5,4 +5,4 @@
 
 opencc -i README.zhs.md -o README.zht.md -c s2twp.json
 sed -i 's/\[中文繁體\](README\.zht\.md)/[中文简体](README.zhs.md)/' ./README.zht.md
-sed -i '/「整」專案/a\\n  *本文由 opencc 轉換得*' ./README.zht.md
+sed -i '/「整」專案/a\\n> *本文由 opencc 轉換得*' ./README.zht.md
